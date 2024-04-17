e8df014d968a18987ecf3823de7663687716a8d9
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-04-17 15:41:50 +0700

    feat: add location icon

--- src/components/common/AddressCard/index.tsx
+++ src/components/common/AddressCard/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { ChevronDown } from 'lucide-react';
+import { LocationIcon } from '@/assets/icons';
 
 const AddressCard = () => {
   return (
     <div className="text-primary-dark bg-primary-light border border-primary-border rounded-lg px-4 py-3 flex items-center justify-between cursor-pointer">
-      2640 Cabin Creek Rd #102 Alexandria, Virginia(VA), 22314
-      <ChevronDown />
+      <div className="flex gap-3">
+        <LocationIcon />
+        2640 Cabin Creek Rd #102 Alexandria, Virginia(VA), 22314
+      </div>
+      <span>
+        <ChevronDown />
+      </span>
     </div>
   );
 };
