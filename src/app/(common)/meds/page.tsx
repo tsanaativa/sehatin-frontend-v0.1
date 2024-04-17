1e9cbdd08806c15a18842b0894b487930a3dedfd
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-04-17 22:29:40 +0700

    fix: fix see all url, svg props

--- src/app/(common)/meds/page.tsx
+++ src/app/(common)/meds/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/common';
-import AddressCard from '@/components/common/AddressCard';
 import CategoriesSection from '@/components/common/CategoriesSection';
 import ProductsSection from '@/components/common/ProductsSection';
 import React from 'react';
@@ -8,14 +7,8 @@ const Meds = () => {
   return (
     <div className="flex flex-col gap-6">
       <CategoriesSection />
-      <ProductsSection
-        title="Vitamin C"
-        seeAllUrl="/meds/categories/vitamin-c"
-      />
-      <ProductsSection
-        title="Obat Sakit Kepala"
-        seeAllUrl="/meds/categories/obat-sakit-kepala"
-      />
+      <ProductsSection title="Vitamin C" seeAllUrl="/meds/category/1" />
+      <ProductsSection title="Obat Sakit Kepala" seeAllUrl="/meds/category/2" />
       <Button className="w-full mb-6" variant="outlined-primary">
         Load More
       </Button>
