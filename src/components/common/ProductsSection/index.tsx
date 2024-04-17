21f0da7a894562d2b6eb93ee86646c3454d135d4
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-04-17 15:41:21 +0700

    feat: add products section

---
+++ src/components/common/ProductsSection/index.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import React from 'react';
+import { ProductCard } from '..';
+
+type ProductsSectionProps = {
+  title: string;
+  seeAllUrl: string;
+};
+
+const ProductsSection = ({ title, seeAllUrl }: ProductsSectionProps) => {
+  return (
+    <section>
+      <div className="flex items-center justify-between">
+        <span className="font-poppins font-semibold text-dark">{title}</span>
+        <Link className="text-primary-dark text-sm" href={seeAllUrl}>
+          See all
+        </Link>
+      </div>
+      <div className="overflow-x-auto">
+        <div className="grid grid-cols-6 min-w-max gap-4 mt-2">
+          {Array.from({ length: 6 }).map((val, idx) => (
+            <ProductCard key={idx} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProductsSection;
