745e092543b0e21f961d2b11a9c596fa9299b6aa
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-04-18 22:16:00 +0700

    fix: fix gap products section

--- src/components/common/ProductsSection/index.tsx
+++ src/components/common/ProductsSection/index.tsx
@@ -19,7 +19,7 @@ const ProductsSection = ({ title, seeAllUrl }: ProductsSectionProps) => {
         </Link>
       </div>
       <div className="overflow-x-auto">
-        <div className="grid grid-cols-6 min-w-max gap-4 mt-2 md:mt-4">
+        <div className="grid grid-cols-6 min-w-max gap-3 sm:gap-4 md:gap-6 mt-2 md:mt-4">
           {Array.from({ length: 6 }).map((val, idx) => (
             <ProductCard key={idx} />
           ))}
