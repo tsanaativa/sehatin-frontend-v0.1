22cf11534c244baf04d8fe9f69a7d0a11242bc38
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-04-18 22:16:49 +0700

    feat: add responsivenesslayout meds

--- src/app/(common)/meds/layout.tsx
+++ src/app/(common)/meds/layout.tsx
@@ -6,13 +6,13 @@ const MedsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-primary-dark bg-gradient-to-r from-slate-500/0 to-primary">
       <div className="flex justify-center px-4">
-        <div className="py-4 relative flex flex-col gap-4 text-sm w-full lg:max-w-[1100px]">
+        <div className="py-4 relative flex flex-col gap-4 text-sm w-full lg:max-w-[1075px]">
           <AddressCard />
           <SearchBar />
         </div>
       </div>
       <div className="w-full bg-light rounded-tr-2xl rounded-tl-2xl md:rounded-none flex justify-center px-4">
-        <div className="py-4 md:py-10 w-full lg:max-w-[1100px]">{children}</div>
+        <div className="py-4 md:py-10 w-full lg:max-w-[1075px]">{children}</div>
       </div>
     </div>
   );
