be53b0beb3c4a1119031b9679930b6d43a36afaf
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-04-18 13:44:11 +0700

    fix: fix export common components

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,8 @@
 export { default as Button } from './Button';
 export { default as Carousel } from './Carousel';
 export { default as ProductCard } from './ProductCard';
+export { default as CategoriesSection } from './CategoriesSection';
+export { default as ProductsSection } from './ProductsSection';
+export { default as SortDropdown } from './SortDropdown';
+export { default as FilterDropdown } from './FilterDropdown';
+export { default as Pagination } from './Pagination';
---
+++ src/features/meds/components/index.ts
@@ -0,0 +1,2 @@
+export { default as AddressCard } from './AddressCard';
+export { default as SearchBar } from './SearchBar';
