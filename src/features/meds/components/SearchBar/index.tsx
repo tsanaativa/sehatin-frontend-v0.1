527918487071b57883db077bf9e81b3d03a76893
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-04-18 10:33:35 +0700

    feat: usable search bar

--- src/features/meds/components/SearchBar/index.tsx
+++ src/features/meds/components/SearchBar/index.tsx
@@ -1,15 +1,30 @@
+'use client';
+
 import { Search } from 'lucide-react';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useRef } from 'react';
 
 const SearchBar = () => {
+  const router = useRouter();
+  const searchBarRef = useRef<HTMLInputElement>(null);
+
+  function handleKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
+    let value = searchBarRef.current?.value;
+    if (e.key === 'Enter') {
+      router.push(`/meds/search?keyword=${value}`);
+    }
+  }
+
   return (
     <div className="w-full bg-light rounded-lg flex items-center">
       <div className="text-gray px-3">
         <Search size={20} />
       </div>
       <input
+        ref={searchBarRef}
         className="w-full py-3 outline-none border-0 rounded-lg"
         placeholder="Search..."
+        onKeyUp={handleKeyUp}
       />
     </div>
   );
