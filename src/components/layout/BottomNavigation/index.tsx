1880dd37fa13fc474ca45309f33cc60cad7f46a9
riyyanfirdaus <m.riyyan.firdaus@gmail.com>
2024-04-19 17:27:49 +0700

    feat: update BottomNavigation component

--- src/components/layout/BottomNavigation/index.tsx
+++ src/components/layout/BottomNavigation/index.tsx
@@ -1,71 +1,51 @@
 'use client';
-import {
-  ClipboardList,
-  Home,
-  MessageSquareMore,
-  Pill,
-  UserRound,
-} from 'lucide-react';
+import { Icon } from '@/components/common';
+import { MENUS } from '@/constants/menus';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 const BottomNavigation = () => {
   const pathname = usePathname();
+  const listOfLink = MENUS.map((val) => val.link);
+  const isPage = listOfLink.includes(pathname);
+
   return (
-    <div className="fixed bottom-0 w-full bg-light rounded-tl-lg rounded-tr-lg border-t-2 border-t-gray-lighter md:hidden">
-      <ul className="flex items-center justify-between px-6 py-3">
-        <li>
-          <Link
-            className={`flex flex-col items-center gap-y-1 ${
-              pathname === '/' ? 'text-primary-dark' : 'text-dark-gray'
-            }`}
-            href="/"
-          >
-            <Home />
-            <span>Home</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={`flex flex-col items-center gap-y-1 ${
-              pathname === '/meds' ? 'text-primary-dark' : 'text-dark-gray'
-            }`}
-            href="/meds"
-          >
-            <Pill />
-            <span>Meds</span>
-          </Link>
-        </li>
-        <li className="flex flex-col items-center relative">
-          <div className="absolute -top-12 bg-primary text-white-fa p-4 rounded-full border-8 border-primary-outline">
-            <MessageSquareMore />
-          </div>
-          <span className="mt-8 text-dark-gray">Consult</span>
-        </li>
-        <li>
-          <Link
-            className={`flex flex-col items-center gap-y-1 ${
-              pathname === '/orders' ? 'text-primary-dark' : 'text-dark-gray'
-            }`}
-            href="/orders"
-          >
-            <ClipboardList />
-            <span>Orders</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={`flex flex-col items-center gap-y-1 ${
-              pathname === '/profile' ? 'text-primary-dark' : 'text-dark-gray'
-            }`}
-            href="/profile"
-          >
-            <UserRound />
-            <span>Profile</span>
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      {isPage ? (
+        <div className="fixed bottom-0 w-full bg-light rounded-tl-lg rounded-tr-lg border-t-2 border-t-gray-lighter md:hidden">
+          <ul className="flex items-center justify-between px-6 py-3">
+            {MENUS.map((menu, idx) => {
+              if (menu.label === 'Consult') {
+                return (
+                  <li className="flex flex-col items-center relative" key={idx}>
+                    <div className="absolute -top-12 bg-primary text-white-fa p-4 rounded-full border-8 border-primary-outline">
+                      <Icon name={menu.icon} />
+                    </div>
+                    <span className="mt-8 text-dark-gray">Consult</span>
+                  </li>
+                );
+              }
+
+              return (
+                <li key={idx}>
+                  <Link
+                    className={`flex flex-col items-center gap-y-1 ${
+                      pathname === menu.link
+                        ? 'text-primary-dark'
+                        : 'text-dark-gray'
+                    }`}
+                    href={menu.link}
+                  >
+                    <Icon name={menu.icon} />
+                    <span>{menu.label}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ) : null}
+    </>
   );
 };
 
