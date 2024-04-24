9d6e9ba577653bf54f176980a3be2015a77289e6
riyyanfirdaus <m.riyyan.firdaus@gmail.com>
2024-04-24 09:54:02 +0700

    feat: add styling desktop version

--- src/features/meds/components/ChoosePharmaciesList/index.tsx
+++ src/features/meds/components/ChoosePharmaciesList/index.tsx
@@ -1,6 +1,6 @@
 import Badge from '@/components/common/Badge';
 import { DUMMY_CHOOSE_PHARMACIES } from '@/constants/dummy';
-import { MapPin } from 'lucide-react';
+import { Check, MapPin } from 'lucide-react';
 
 const ChoosePharmaciesList = () => {
   return (
@@ -14,10 +14,14 @@ const ChoosePharmaciesList = () => {
             className={`peer/pharmacy-${idx}`}
             hidden
           />
-          <label htmlFor={`pharmacy-${idx}`}>
+          <label htmlFor={`pharmacy-${idx}`} className="cursor-pointer">
             <div className="flex items-center justify-between border-b border-primary-border px-4 py-2">
               <span>{item.name}</span>
-              {item.status ? <Badge>Selected</Badge> : null}
+              {item.status ? (
+                <Badge variant="success">
+                  <Check size={18} /> Selected
+                </Badge>
+              ) : null}
             </div>
             <div className="flex justify-between gap-x-4 px-4 py-2">
               <MapPin className="text-dark-gray" />
