77dcbbda284e3be05ce8a164aa163d5c3a52d324
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-04-25 11:54:59 +0700

    feat: implement address form

---
+++ src/components/common/AddressForm/index.tsx
@@ -0,0 +1,24 @@
+const AddressForm = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-4">
+      <div className="flex flex-col">
+        <label>Province</label>
+        <input className="border border-gray-light w-full" />
+      </div>
+      <div className="flex flex-col">
+        <label>Province</label>
+        <input className="border border-gray-light w-full" />
+      </div>
+      <div className="flex flex-col">
+        <label>Province</label>
+        <input className="border border-gray-light w-full" />
+      </div>
+      <div className="flex flex-col">
+        <label>Province</label>
+        <input className="border border-gray-light w-full" />
+      </div>
+    </div>
+  );
+};
+
+export default AddressForm;
--- src/features/profile/components/AddAddressModalButton/ModalAddAddress/index.tsx
+++ src/features/profile/components/AddAddressModalButton/ModalAddAddress/index.tsx
@@ -1,4 +1,5 @@
 import { Button, Modal } from '@/components/common';
+import AddressForm from '@/components/common/AddressForm';
 import { X } from 'lucide-react';
 
 type ModalAddAddressProps = {
@@ -16,35 +17,12 @@ const ModalAddAddress = ({ onShowModal, showModal }: ModalAddAddressProps) => {
           onClick={() => onShowModal(false)}
         />
       </div>
-      <div className="flex flex-col gap-y-2 px-4 py-4 md:min-w-[500px]">
+      <div className="flex flex-col gap-y-2 px-4 py-4 md:min-w-[700px]">
         <div className="flex flex-col gap-4 text-xs text-dark-gray md:text-base">
-          <div>
-            <label>Password</label>
-            <br />
-            <input
-              type="password"
-              className="text-dark border border-gray-light p-2 w-full"
-            />
-          </div>
-          <div>
-            <label>New Password</label>
-            <br />
-            <input
-              type="password"
-              className="text-dark border border-gray-light p-2 w-full"
-            />
-          </div>
-          <div>
-            <label>Confirm New Password</label>
-            <br />
-            <input
-              type="password"
-              className="text-dark border border-gray-light p-2 w-full"
-            />
-          </div>
+          <AddressForm />
         </div>
         <Button className="flex items-center justify-center gap-x-2 mt-6">
-          Change Password
+          Save
         </Button>
       </div>
     </Modal>
