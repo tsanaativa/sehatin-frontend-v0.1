5a4b483937f90c8e5f166eb98d6fbeb74ea5ed12
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-04-26 17:15:23 +0700

    fix: fix gap profile

--- src/app/(common)/profile/my-profile/page.tsx
+++ src/app/(common)/profile/my-profile/page.tsx
@@ -8,7 +8,7 @@ const MyProfile = () => {
       <h2 className="text-xl text-center font-semibold font-poppins md:text-start md:text-2xl">
         My Profile
       </h2>
-      <div className="flex flex-col items-center gap-10 mt-5 md:flex-row md:items-start">
+      <div className="flex flex-col items-center gap-5 md:gap-10 mt-5 md:flex-row md:items-start">
         <AvatarUploader />
         <div className="w-full">
           <ProfileForm defaultUser={DUMMY_USER} />
