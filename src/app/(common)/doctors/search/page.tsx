d514cc1529f616800abadccfdf8a8bd50791be7d
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-05-01 11:41:43 +0700

    fix: erase unneeded comment

--- src/app/(common)/doctors/search/page.tsx
+++ src/app/(common)/doctors/search/page.tsx
@@ -105,21 +105,6 @@ const SearchDoctors = () => {
     [pathname, replace, searchParams]
   );
 
-  // useEffect(() => {
-  //   const newParams = new URLSearchParams(searchParams);
-  //   Object.keys(params).map((key) => {
-  //     if (key !== 'limit') {
-  //       let val = params[key as keyof typeof params].toString();
-  //       if (val !== '') {
-  //         newParams.set(key, val);
-  //       } else {
-  //         newParams.delete(key);
-  //       }
-  //       replace(`${pathname}?${newParams.toString()}`);
-  //     }
-  //   });
-  // }, [params, pathname, replace, searchParams]);
-
   return (
     <div className="w-full bg-light rounded-tr-2xl rounded-tl-2xl flex justify-center px-1 md:px-6 md:rounded-none">
       <div className="max-w-[1150px] py-4 w-full px-4 sm:px-6 md:py-10">
