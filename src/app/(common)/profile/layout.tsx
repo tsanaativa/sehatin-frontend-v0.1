f11f352ed5ee29606f9b804c7a6d7aaf27ebabe4
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-05-03 00:22:34 +0700

    fix: fix profile sidebar

--- src/app/(common)/profile/layout.tsx
+++ src/app/(common)/profile/layout.tsx
@@ -1,12 +1,15 @@
 import ProfileSidebar from '@/features/profile/components/ProfileSidebar';
+import { getUser } from '@/utils/user';
 import React from 'react';
 
 const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
+  const user = getUser();
+
   return (
     <div className="w-full min-h-[calc(100vh-4rem)] max-w-[1440px] flex justify-center md:min-h-fit">
       <div className="w-full flex gap-6 pt-6 pb-8 px-4 max-w-[1150px] sm:px-6 md:py-12">
         <div className="hidden md:block">
-          <ProfileSidebar />
+          <ProfileSidebar user={user} />
         </div>
         <div className="w-full min-h-full md:border md:border-gray-light md:py-6 md:px-7 md:rounded">
           {children}
