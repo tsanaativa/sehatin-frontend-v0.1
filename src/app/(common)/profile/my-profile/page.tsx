c52fbd0ca722c4eaef2ed2663e7d655a1ee31b3d
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-05-02 18:48:39 +0700

    fix: fix profile

--- src/app/(common)/profile/my-profile/page.tsx
+++ src/app/(common)/profile/my-profile/page.tsx
@@ -1,17 +1,24 @@
-import AvatarUploader from '@/components/common/AvatarUploader';
-import ProfileForm from '@/components/common/ProfileForm';
+import { ProfileForm } from '@/components/common';
+import { getProfile } from '@/services/profile';
+import { LoginData } from '@/types/LoginData';
+import { User } from '@/types/User';
+import cookiesStore from '@/utils/cookies';
 
-const MyProfile = () => {
+const MyProfile = async () => {
+  const user = cookiesStore.get<LoginData>(
+    process.env.NEXT_PUBLIC_USER_LOCAL_KEY || ''
+  )?.user;
+
+  let profileData: User | undefined;
+  profileData = await getProfile(user.id);
+
   return (
     <div>
       <h2 className="text-xl text-center font-semibold font-poppins md:text-start md:text-2xl">
         My Profile
       </h2>
       <div className="flex flex-col items-center gap-5 md:gap-10 mt-5 md:flex-row md:items-start">
-        <AvatarUploader />
-        <div className="w-full">
-          <ProfileForm />
-        </div>
+        <ProfileForm defaultUser={profileData} />
       </div>
     </div>
   );
--- src/components/common/ProfileForm/index.tsx
+++ src/components/common/ProfileForm/index.tsx
@@ -1,11 +1,12 @@
 'use client';
-import React, { useRef, useState } from 'react';
-import { Button, Input } from '@/components/common';
+
+import { AvatarUploader, Button, Input } from '@/components/common';
 import Selector from '@/components/common/Selector';
-import { validate } from '@/utils/validation';
 import { DUMMY_SPECIALISTS } from '@/constants/dummy';
 import { Doctor } from '@/types/Doctor';
 import { User } from '@/types/User';
+import { validate } from '@/utils/validation';
+import { useRef, useState } from 'react';
 import ChangePasswordButton from '../ChangePasswordButton';
 
 type ProfileFormProps = {
@@ -204,204 +205,209 @@ const ProfileForm = ({
   };
 
   return (
-    <div>
-      <form
-        action={handleSubmit}
-        className="flex flex-col gap-4 [&>label]:flex [&>label]:flex-col [&>label]:gap-1 [&_h5]:text-[14px] [&_h5]:text-dark-gray [&_h5]:leading-[150%]"
-      >
-        <label htmlFor="name">
-          <h5>Name</h5>
-          <Input
-            ref={name}
-            id="name"
-            name="name"
-            type="text"
-            placeholder="Type your fullname ..."
-            message={errors['all']['name']}
-            invalid={errors['all']['name'] !== ''}
-            onInput={({ target }) =>
-              handleInput('all', 'name', (target as HTMLInputElement).value)
-            }
-            defaultValue={
-              role === 'doctor' ? defaultDoctor?.name : defaultUser?.name
-            }
-          />
-        </label>
-        <label htmlFor="email">
-          <h5>Email</h5>
-          <Input
-            ref={email}
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Type your email ..."
-            message={errors['all']['email']}
-            invalid={errors['all']['email'] !== ''}
-            onInput={({ target }) =>
-              handleInput('all', 'email', (target as HTMLInputElement).value)
-            }
-            defaultValue={
-              role === 'doctor' ? defaultDoctor?.email : defaultUser?.email
-            }
-          />
-        </label>
-        {role == 'user' && (
-          <>
-            <label htmlFor="birth-date">
-              <h5>Birth Date</h5>
-              <Input
-                ref={birthDatePicker}
-                id="birth-date"
-                name="birth-date"
-                placeholder="Enter your birth date ..."
-                append="Calendar"
-                type="date"
-                valueMode={birthDate}
-                onInput={({ target }) => handleDate(target as HTMLInputElement)}
-                message={errors['user']['birth-date']}
-                invalid={errors['user']['birth-date'] !== ''}
-                onAppend={() => {
-                  birthDatePicker.current?.focus();
-                  birthDatePicker.current?.showPicker();
-                }}
-                defaultValue={defaultUser?.birth_date}
-              />
-            </label>
-            <div>
-              <h5>Gender</h5>
-              <div
-                className="flex gap-16 items-center [&>label]:flex [&>label]:cursor-pointer [&>label]:items-center [&>label]:gap-1.5 [&_input]:hidden
+    <>
+      <AvatarUploader defaultAvatar={defaultUser?.profile_picture} />
+      <div className="w-full">
+        <form
+          action={handleSubmit}
+          className="flex flex-col gap-4 [&>label]:flex [&>label]:flex-col [&>label]:gap-1 [&_h5]:text-[14px] [&_h5]:text-dark-gray [&_h5]:leading-[150%]"
+        >
+          <label htmlFor="name">
+            <h5>Name</h5>
+            <Input
+              ref={name}
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Type your fullname ..."
+              message={errors['all']['name']}
+              invalid={errors['all']['name'] !== ''}
+              onInput={({ target }) =>
+                handleInput('all', 'name', (target as HTMLInputElement).value)
+              }
+              defaultValue={
+                role === 'doctor' ? defaultDoctor?.name : defaultUser?.name
+              }
+            />
+          </label>
+          <label htmlFor="email">
+            <h5>Email</h5>
+            <Input
+              ref={email}
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Type your email ..."
+              message={errors['all']['email']}
+              invalid={errors['all']['email'] !== ''}
+              onInput={({ target }) =>
+                handleInput('all', 'email', (target as HTMLInputElement).value)
+              }
+              defaultValue={
+                role === 'doctor' ? defaultDoctor?.email : defaultUser?.email
+              }
+            />
+          </label>
+          {role == 'user' && (
+            <>
+              <label htmlFor="birth-date">
+                <h5>Birth Date</h5>
+                <Input
+                  ref={birthDatePicker}
+                  id="birth-date"
+                  name="birth-date"
+                  placeholder="Enter your birth date ..."
+                  append="Calendar"
+                  type="date"
+                  valueMode={birthDate}
+                  onInput={({ target }) =>
+                    handleDate(target as HTMLInputElement)
+                  }
+                  message={errors['user']['birth-date']}
+                  invalid={errors['user']['birth-date'] !== ''}
+                  onAppend={() => {
+                    birthDatePicker.current?.focus();
+                    birthDatePicker.current?.showPicker();
+                  }}
+                  defaultValue={defaultUser?.birth_date}
+                />
+              </label>
+              <div>
+                <h5>Gender</h5>
+                <div
+                  className="flex gap-16 items-center [&>label]:flex [&>label]:cursor-pointer [&>label]:items-center [&>label]:gap-1.5 [&_input]:hidden
               [&_mark]:grid [&_mark]:place-items-center [&_mark]:w-4 [&_mark]:h-4 [&_mark]:rounded-full [&_mark]:border-[1px] [&_mark]:border-gray
               [&_mark]:bg-transparent after:[&_mark]:content-[''] after:[&_mark]:h-2.5 after:[&_mark]:w-2.5 after:[&_mark]:rounded-full
               after:[&_mark]:bg-primary-dark after:[&_mark]:hidden [&_span]:leading-[150%] [&_span]:tracking-[0.5px]"
-              >
-                <label htmlFor="male">
-                  <input
-                    type="radio"
-                    name="gender"
-                    id="male"
-                    className="peer"
-                    checked={gender == 'male'}
-                    onChange={() => setGender('male')}
-                  />
-                  <mark className="peer-checked:after:block peer-checked:border-primary-dark"></mark>
-                  <span>Male</span>
-                </label>
-                <label htmlFor="female">
-                  <input
-                    type="radio"
-                    name="gender"
-                    id="female"
-                    className="peer"
-                    checked={gender == 'female'}
-                    onChange={() => setGender('female')}
-                  />
-                  <mark className="peer-checked:after:block peer-checked:border-primary-dark"></mark>
-                  <span>Female</span>
-                </label>
-              </div>
-            </div>
-          </>
-        )}
-        {role == 'doctor' && (
-          <>
-            <label htmlFor="specialist">
-              <h5>Specialist</h5>
-              <Selector
-                id="specialist"
-                options={specialistOptions}
-                selected={specialty}
-                name="specialist"
-                searchable
-                required
-                onSelect={handleSpecialty}
-                invalid={errors['doctor']['specialist'] !== ''}
-                message={errors['doctor']['specialist']}
-                placeholder="Choose your specialty ..."
-              />
-            </label>
-            <label htmlFor="consultation-fee">
-              <h5>Consultation Fee</h5>
-              <Input
-                ref={consultationFee}
-                id="consultation-fee"
-                name="consultation-fee"
-                placeholder="Type your prefered consultation fee ..."
-                prepend="Rp"
-                type="number"
-                invalid={errors['doctor']['consultation-fee-number'] !== ''}
-                message={errors['doctor']['consultation-fee-number']}
-                onInput={({ target }) =>
-                  handleInput(
-                    'doctor',
-                    'consultation-fee-number',
-                    (target as HTMLInputElement).value
-                  )
-                }
-                defaultValue={defaultDoctor?.fee}
-              />
-            </label>
-            <label htmlFor="work-start">
-              <h5>Work Start</h5>
-              <Selector
-                id="work-start"
-                options={workStartOptions}
-                selected={workStart}
-                name="work-start"
-                invalid={errors['doctor']['work-start'] !== ''}
-                message={errors['doctor']['work-start']}
-                onSelect={handleWorkStart}
-                append="Calendar"
-                placeholder="Choose the year you start working ..."
-              />
-            </label>
-            <label htmlFor="certificate">
-              <h5>Doctor Certificate</h5>
-              <div className="flex items-center gap-3">
-                <input
-                  ref={certificate}
-                  type="file"
-                  name="certificate"
-                  id="certificate"
-                  className="hidden"
-                  accept="application/pdf"
-                  onInput={({ target }) => {
-                    handleCertificate(target as HTMLInputElement);
-                    (target as HTMLInputElement).value = '';
-                  }}
-                />
-                <button
-                  type="button"
-                  onClick={() => certificate.current?.click()}
-                  className="font-poppins text-primary border-[1px] text-sm min-w-32 border-primary rounded-md h-10 px-3 leading-[150%] hover:border-primary-dark hover:text-primary-dark transition-colors duration-300"
                 >
-                  {filename.length > 0 ? 'Change' : 'Choose'} File...
-                </button>
-                <p className="text-xs leading-[150%] text-dark-gray overflow-hidden [display:-webkit-box] [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">
-                  {filename.length > 0 ? filename : 'PDF format, max 1 MB'}
-                </p>
+                  <label htmlFor="male">
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="male"
+                      className="peer"
+                      checked={gender == 'male'}
+                      onChange={() => setGender('male')}
+                    />
+                    <mark className="peer-checked:after:block peer-checked:border-primary-dark"></mark>
+                    <span>Male</span>
+                  </label>
+                  <label htmlFor="female">
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="female"
+                      className="peer"
+                      checked={gender == 'female'}
+                      onChange={() => setGender('female')}
+                    />
+                    <mark className="peer-checked:after:block peer-checked:border-primary-dark"></mark>
+                    <span>Female</span>
+                  </label>
+                </div>
               </div>
-              {errors['doctor']['doctor-certificate'].length > 0 && (
-                <span className={`text-danger text-xs`}>
-                  {errors['doctor']['doctor-certificate']}
-                </span>
-              )}
-            </label>
-          </>
-        )}
-        <div className="flex justify-end items-center">
-          <Button
-            variant="outlined-primary"
-            className="flex items-center justify-center gap-1 px-6 min-w-[150px] mt-3 w-fit"
-          >
-            Save
-          </Button>
+            </>
+          )}
+          {role == 'doctor' && (
+            <>
+              <label htmlFor="specialist">
+                <h5>Specialist</h5>
+                <Selector
+                  id="specialist"
+                  options={specialistOptions}
+                  selected={specialty}
+                  name="specialist"
+                  searchable
+                  required
+                  onSelect={handleSpecialty}
+                  invalid={errors['doctor']['specialist'] !== ''}
+                  message={errors['doctor']['specialist']}
+                  placeholder="Choose your specialty ..."
+                />
+              </label>
+              <label htmlFor="consultation-fee">
+                <h5>Consultation Fee</h5>
+                <Input
+                  ref={consultationFee}
+                  id="consultation-fee"
+                  name="consultation-fee"
+                  placeholder="Type your prefered consultation fee ..."
+                  prepend="Rp"
+                  type="number"
+                  invalid={errors['doctor']['consultation-fee-number'] !== ''}
+                  message={errors['doctor']['consultation-fee-number']}
+                  onInput={({ target }) =>
+                    handleInput(
+                      'doctor',
+                      'consultation-fee-number',
+                      (target as HTMLInputElement).value
+                    )
+                  }
+                  defaultValue={defaultDoctor?.fee}
+                />
+              </label>
+              <label htmlFor="work-start">
+                <h5>Work Start</h5>
+                <Selector
+                  id="work-start"
+                  options={workStartOptions}
+                  selected={workStart}
+                  name="work-start"
+                  invalid={errors['doctor']['work-start'] !== ''}
+                  message={errors['doctor']['work-start']}
+                  onSelect={handleWorkStart}
+                  append="Calendar"
+                  placeholder="Choose the year you start working ..."
+                />
+              </label>
+              <label htmlFor="certificate">
+                <h5>Doctor Certificate</h5>
+                <div className="flex items-center gap-3">
+                  <input
+                    ref={certificate}
+                    type="file"
+                    name="certificate"
+                    id="certificate"
+                    className="hidden"
+                    accept="application/pdf"
+                    onInput={({ target }) => {
+                      handleCertificate(target as HTMLInputElement);
+                      (target as HTMLInputElement).value = '';
+                    }}
+                  />
+                  <button
+                    type="button"
+                    onClick={() => certificate.current?.click()}
+                    className="font-poppins text-primary border-[1px] text-sm min-w-32 border-primary rounded-md h-10 px-3 leading-[150%] hover:border-primary-dark hover:text-primary-dark transition-colors duration-300"
+                  >
+                    {filename.length > 0 ? 'Change' : 'Choose'} File...
+                  </button>
+                  <p className="text-xs leading-[150%] text-dark-gray overflow-hidden [display:-webkit-box] [-webkit-line-clamp:2] [-webkit-box-orient:vertical]">
+                    {filename.length > 0 ? filename : 'PDF format, max 1 MB'}
+                  </p>
+                </div>
+                {errors['doctor']['doctor-certificate'].length > 0 && (
+                  <span className={`text-danger text-xs`}>
+                    {errors['doctor']['doctor-certificate']}
+                  </span>
+                )}
+              </label>
+            </>
+          )}
+          <div className="flex justify-end items-center">
+            <Button
+              variant="outlined-primary"
+              className="flex items-center justify-center gap-1 px-6 min-w-[150px] mt-3 w-fit"
+            >
+              Save
+            </Button>
+          </div>
+        </form>
+        <div className="pb-6 -mt-8 md:-mt-9">
+          <ChangePasswordButton />
         </div>
-      </form>
-      <div className="pb-6 -mt-8 md:-mt-9">
-        <ChangePasswordButton />
       </div>
-    </div>
+    </>
   );
 };
 
