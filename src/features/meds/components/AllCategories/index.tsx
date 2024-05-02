ea982c48a77ea9920481e79ed0bff42f8faedf14
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-05-02 18:44:40 +0700

    feat: fix categories to services

--- src/features/meds/components/AllCategories/index.tsx
+++ src/features/meds/components/AllCategories/index.tsx
@@ -1,43 +1,21 @@
-'use client';
-
 import NoDataFound from '@/components/common/NoDataFound';
 import Skeleton from '@/components/common/Skeleton';
+import { getCategories } from '@/services/category';
 import { Category } from '@/types/Product';
-import api from '@/utils/api';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { toast } from 'react-toastify';
 
-const AllCategories = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const params = {};
-        const res = await api.get<typeof params, { categories: Category[] }>(
-          `/categories`
-        );
-        setCategories(res.data.categories);
-      } catch (error: any) {
-        toast.error(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchCategories();
-  }, []);
+const AllCategories = async () => {
+  let categories: Category[] | undefined;
+  categories = await getCategories();
 
   return (
     <div>
-      {isLoading ? (
+      {!!!categories ? (
         <div className="bg-light rounded-lg border border-gray-light mt-3 flex flex-col md:grid md:grid-cols-3 md:grid-flow-row md:mt-5 md:text-lg">
           {Array.from({ length: 30 }).map((val, idx) => (
             <div
               key={idx}
-              className={`border-gray-light px-3 py-2 ${idx + 1 !== categories.length && 'border-b md:border-b-0'} md:py-3 md:px-4 ${idx % 3 !== 2 && 'md:border-e'}`}
+              className={`border-gray-light px-3 py-2 ${idx + 1 !== categories?.length && 'border-b md:border-b-0'} md:py-3 md:px-4 ${idx % 3 !== 2 && 'md:border-e'}`}
             >
               <Skeleton>
                 <div className="bg-gray-light">
@@ -55,7 +33,7 @@ const AllCategories = () => {
                 <Link key={idx} href={`/meds/search?categoryId=${category.id}`}>
                   <div
                     key={idx}
-                    className={`border-gray-light px-3 py-2 h-full ${idx + 1 !== categories.length && 'border-b md:border-b-0'} md:py-3 md:px-4 ${idx % 3 !== 2 && 'md:border-e'}`}
+                    className={`border-gray-light px-3 py-2 h-full ${idx + 1 !== categories?.length && 'border-b md:border-b-0'} md:py-3 md:px-4 ${idx % 3 !== 2 && 'md:border-e'}`}
                   >
                     {category.name}
                   </div>
@@ -65,7 +43,7 @@ const AllCategories = () => {
                 (val, idx) => (
                   <div
                     key={idx}
-                    className={`hidden border-gray-light py-3 px-4 h-full ${idx !== 2 - (categories.length % 3) && 'border-e'} md:block`}
+                    className={`hidden border-gray-light py-3 px-4 h-full ${idx !== 2 - ((categories?.length || 0) % 3) && 'border-e'} md:block`}
                   ></div>
                 )
               )}
